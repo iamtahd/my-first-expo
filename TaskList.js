80387398b8bb2986120b8b4806d5519356a9178c
Todd <iamtahd@gmail.com>
2020-05-11 20:32:21 -0400

    commit before upgrading dev dependices

--- TaskList.js
+++ TaskList.js
@@ -1,37 +1,11 @@
 import React from 'react';
-import PropTypes from 'prop-types'
-import { FlatList, View, StyleSheet } from 'react-native';
-
-const styles = StyleSheet.create({
-    container: {
-        paddingTop: 40
-    },
-});
-
-const renderItem = ({ item, index, todos, setTodo}) => {
-    return <Row
-        title={item}
-        isF
-    />
-}
+import { Text } from 'react-native';
 
 const TaskList = () => {
-    const [todos, setTodo] = React.useState([]);
-    const renderItemCall = useCallback(({item, index}) => renderItem({item, index, todos, setTodo}));
-    return (
-        <View style={styles.container}>
-            <FlatList
-                extraData={todos}
-                data={data}
-                renderItem={renderItemCall}
-                keyExtractor={keyExtractor}
-            />
-        </View>
-    );
+  return (
+      <Text>This is an update</Text>
+  );
 };
 
-// TaskList.propTypes = {
-//     todos: PropTypes.arrayOf(PropTypes.object).isRequired
-// };
 
 export default TaskList;
